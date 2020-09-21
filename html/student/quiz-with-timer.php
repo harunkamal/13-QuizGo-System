07db2af911c81618b47bffd9b67f2c26af38b198
syd <syd.shiddarta@icloud.com>
2020-09-21 18:01:04 +0800

    Section Message Insertion

--- html/student/quiz-with-timer.php
+++ html/student/quiz-with-timer.php
@@ -9,7 +9,7 @@ date_default_timezone_set("Asia/Kuala_Lumpur");
 
 
 $modify_quizes = read("quiz");
-$timer = "10000"; // Changed Timeframe 
+$timer = "1000"; // Changed Timeframe 
 
   $id = 14;
   foreach ($modify_quizes as $modify_quize) {
@@ -56,7 +56,7 @@ $timer = "10000"; // Changed Timeframe
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Good luck</h1>
+            <h1 class="h2">Good luck in the Quiz</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
               </div>
@@ -146,7 +146,7 @@ $timer = "10000"; // Changed Timeframe
 
 
 
-          }, 10000);
+          }, 1000);
 
 
         </script>
